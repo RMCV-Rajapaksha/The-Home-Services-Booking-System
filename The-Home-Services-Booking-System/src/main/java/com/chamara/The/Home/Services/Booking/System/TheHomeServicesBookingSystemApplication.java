a26198819a73bff32db5b25824c02c236edd1417
RMCV-Rajapaksha <cam.chamara@gmail.com>
2024-12-22 10:49:24 +0530

    first commit

---
+++ The-Home-Services-Booking-System/src/main/java/com/chamara/The/Home/Services/Booking/System/TheHomeServicesBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.chamara.The.Home.Services.Booking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHomeServicesBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHomeServicesBookingSystemApplication.class, args);
+	}
+
+}
---
+++ The-Home-Services-Booking-System/src/test/java/com/chamara/The/Home/Services/Booking/System/TheHomeServicesBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamara.The.Home.Services.Booking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHomeServicesBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
