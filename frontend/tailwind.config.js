5e74e29ed3f8d7439d8c5e7bb2b236fcc1f6e9d2
RMCV-Rajapaksha <cam.chamara@gmail.com>
2024-12-26 20:25:40 +0530

    Update index.html title and enhance Tailwind CSS configuration with custom colors and font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#111B47",
+        secondary: "rgba(6, 62, 85, 0.72)",
+        "secondary-1": "#063E55",
+        tertiary: "#129197",
+      },
+      fontFamily: {
+        body: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
