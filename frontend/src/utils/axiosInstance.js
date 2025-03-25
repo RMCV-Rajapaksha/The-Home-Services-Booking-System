cd3a5d8a7d8d7a1a4bae688d25c1d034045db4d4
RMCV-Rajapaksha <cam.chamara@gmail.com>
2025-03-25 12:20:11 +0530

    Update axios instance base URL to production environment

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://app.devdex.online",
   headers: {
     "Content-Type": "application/json",
   },
