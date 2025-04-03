3871798bb4ff19478f5c2c39747641033d15f854
RMCV-Rajapaksha <cam.chamara@gmail.com>
2025-04-03 16:26:24 +0530

    Refactor serverPorts.tf to simplify EC2 instance outputs and remove unused data sources

--- Codes/Teraform/serverPorts.tf
+++ Codes/Teraform/serverPorts.tf
@@ -1,92 +1,44 @@
-# Configure the AWS provider with explicit credentials and region
+# Terraform configuration to display EC2 instance information
+
+# Configure the AWS provider
 provider "aws" {
-  region  = "eu-north-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  region = "eu-north-1"  # Stockholm region
 }
 
-# Fetch existing EC2 instance by ID
+# Data source to fetch information about the existing EC2 instance
 data "aws_instance" "dev_server" {
   instance_id = "i-08c3cae53b55b50ef"
 }
 
-# Fetch subnet details to get the VPC ID
-data "aws_subnet" "instance_subnet" {
-  id = data.aws_instance.dev_server.subnet_id
-}
-
-# Fetch security group details for the instance
-data "aws_security_group" "instance_sg" {
-  count = length(data.aws_instance.dev_server.vpc_security_group_ids)
-  id    = data.aws_instance.dev_server.vpc_security_group_ids[count.index]
-}
-
-# Output instance details
+# Define outputs to display instance information
 output "instance_id" {
   value = data.aws_instance.dev_server.id
-}
-
-output "instance_name" {
-  value = data.aws_instance.dev_server.tags["Name"]
 }
 
 output "instance_type" {
   value = data.aws_instance.dev_server.instance_type
 }
 
-output "instance_public_dns" {
+output "instance_state" {
+  value = data.aws_instance.dev_server.instance_state
+}
+
+output "public_dns" {
   value = data.aws_instance.dev_server.public_dns
 }
 
-output "instance_public_ip" {
+output "public_ip" {
   value = data.aws_instance.dev_server.public_ip
 }
 
-output "instance_private_ip" {
+output "private_ip" {
   value = data.aws_instance.dev_server.private_ip
 }
 
-output "instance_subnet_id" {
-  value = data.aws_instance.dev_server.subnet_id
-}
-
-output "instance_vpc_id" {
-  value = data.aws_subnet.instance_subnet.vpc_id
-}
-
-output "instance_availability_zone" {
+output "availability_zone" {
   value = data.aws_instance.dev_server.availability_zone
 }
 
-output "instance_ami" {
-  value = data.aws_instance.dev_server.ami
-}
-
-# Output security group details
-output "security_group_details" {
-  value = [
-    for sg in data.aws_security_group.instance_sg : {
-      id          = sg.id
-      name        = sg.name
-      description = sg.description
-      ingress_rules = [
-        for rule in sg.ingress : {
-          from_port   = rule.from_port
-          to_port     = rule.to_port
-          protocol    = rule.protocol
-          cidr_blocks = rule.cidr_blocks
-          description = rule.description
-        }
-      ]
-      egress_rules = [
-        for rule in sg.egress : {
-          from_port   = rule.from_port
-          to_port     = rule.to_port
-          protocol    = rule.protocol
-          cidr_blocks = rule.cidr_blocks
-          description = rule.description
-        }
-      ]
-    }
-  ]
+output "tags" {
+  value = data.aws_instance.dev_server.tags
 }
