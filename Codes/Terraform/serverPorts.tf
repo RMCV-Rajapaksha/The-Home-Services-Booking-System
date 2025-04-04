20c3b1254794ff5660b0b99287458b8836f69724
RMCV-Rajapaksha <cam.chamara@gmail.com>
2025-04-04 06:47:22 +0530

    Add security group rule to allow inbound traffic on port 9000

--- Codes/Terraform/serverPorts.tf
+++ Codes/Terraform/serverPorts.tf
@@ -7,6 +7,22 @@ data "aws_instance" "dev_server" {
   instance_id = "i-08c3cae53b55b50ef"  # The dev-server instance ID
 }
 
+# Get the first security group ID from the instance
+locals {
+  security_group_id = tolist(data.aws_instance.dev_server.vpc_security_group_ids)[0]
+}
+
+# Create a new security group rule to open port 9000
+resource "aws_security_group_rule" "allow_port_9000" {
+  type              = "ingress"
+  from_port         = 9000
+  to_port           = 9000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]  # Allow traffic from anywhere (you may want to restrict this)
+  security_group_id = local.security_group_id
+  description       = "Allow inbound traffic on port 9000"
+}
+
 # Outputs for viewing instance details
 output "instance_id" {
   value = data.aws_instance.dev_server.id
